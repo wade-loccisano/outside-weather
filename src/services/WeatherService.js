00266f6715ad651e3186b626a1a4074973e7c28d
Weighed <wade.loccisano@gmail.com>
2021-02-19 16:22:49 -0500

    Remove API key

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: "https://api.openweathermap.org/data/2.5/weather?zip=16117,us&appid=d964a2d4190c23afe0f06d6e0630134d"
+  baseURL: "https://api.openweathermap.org/data/2.5/weather?zip=16117,us&appid="
 });
 
 export default {
